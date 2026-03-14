4dfa73a486cc0bd71b221800dcb7be9601752cf1
Amrit-Kaur05 <129068474+Amrit-Kaur05@users.noreply.github.com>
2026-03-14 11:53:46 -0400

    Creating user levels of member and officer

--- users/models.py
+++ users/models.py
@@ -10,6 +10,8 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -18,3 +20,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def is_officer(self):
+        return self.user.groups.filter(name='Club Officer').exists()
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- users/views.py
+++ users/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
+from django.contrib.auth.decorators import login_required
+
+# @login_required
+# def dashboard(request):
+#     is_officer = request.user.groups.filter(name='Club Officer').exists()
+#     return render(request, 'dashboard.html', {'is_officer': is_officer})
+
 def profile(request):
-    user = User.objects.get(username="Any")  # This user for now, until login is implemented
-    return render(request, "users/profile.html", {"user": user})
+    is_officer = request.user.groups.filter(name='Club Officer').exists()
+    # user = User.objects.get(username="Any")  # This user for now, until login is implemented
+    return render(request, "users/profile.html", {'is_officer': is_officer})
