fac9fb0a5b6fbfb87311784f0633164b879870e8
mayoeh <mayoeh43@gmail.com>
2026-04-11 14:33:06 -0400

    Fixed logout in nav bar issue

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import LoginView, ProfileView, LogoutView, RegisterView, ChangeRoleView
+from django.contrib.auth import views as auth_views
+from .views import LoginView, ProfileView, LogoutView, RegisterView, ChangeRoleView, RoleAdminView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("change-role/", ChangeRoleView.as_view(), name="change-role"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("role-admin/", RoleAdminView.as_view(), name="role-admin"),
+    path("update-role/<int:user_id>/", ChangeRoleView.as_view(), name="update-role"),
 ]
