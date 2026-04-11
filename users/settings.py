063a679be5052a184bafdad07e04d0c8df2c4bfc
mayoeh <mayoeh43@gmail.com>
2026-04-11 14:33:19 -0400

    Fixed logout in nav bar issue

--- users/settings.py
+++ users/settings.py
@@ -4,3 +4,11 @@ SOCIALACCOUNT_ADAPTER = "users.adapters.CustomSocialAccountAdapter"
 # used https://docs.djangoproject.com/en/6.0/ref/settings/ to look for URL redirection settings
 LOGIN_REDIRECT_URL = "/users/profile/"
 LOGOUT_REDIRECT_URL = "/"
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",  # MUST exist
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",  # MUST exist
+]
