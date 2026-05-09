a3163816b7aa04fefda3e7d6bcbce752e7278ed1
longknguyen <long.nguyen1728@icloud.com>
2026-05-09 01:02:12 -0400

    chore: google oauth

--- cio_website/settings.py
+++ cio_website/settings.py
@@ -183,14 +183,19 @@ AUTHENTICATION_BACKENDS = [
 SOCIALACCOUNT_ADAPTER = "users.adapters.CustomSocialAccountAdapter"
 
 SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
+    "google": {
+        "SCOPE": [
+            "profile",
+            "email",
         ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        }
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        },
+        "APP": {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID", ""),
+            "secret": os.getenv("GOOGLE_CLIENT_SECRET", ""),
+            "key": "",
+        },
     }
 }
 
